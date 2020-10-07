8502eb71ceeb16c833af2bb596e5e05f543bd67c
elhassane <elhassanegargem@gmail.com>
2020-10-07 09:58:29 +0200

    models

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django_countries.fields import CountryField
 
-# Create your models here.
+
+class Brand(models.Model):
+
+    name = models.CharField(max_length=128)
+    founder = models.CharField(max_length=128)
+    country = CountryField()
+
+    def __str__(self):
+        return self.name
+
+
+class Tablet(models.Model):
+
+    name = models.CharField(max_length=128)
+    brand = models.ForeignKey(Brand, null=True, on_delete=models.SET_NULL)
+    storage_size = models.IntegerField(default=0, help_text="Storage size in Go")
+    release_year = models.DateField()
+
+    def __str__(self):
+        return self.name
+
